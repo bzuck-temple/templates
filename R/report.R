2052fe4ce2021aeb6f2d3290bd7b256b12d7f0c7
bzuck <tuc50262@temple.edu>
2020-10-27 22:03:16 -0400

    function for css

---
+++ R/report.R
@@ -0,0 +1,11 @@
+JamiesStyle <- function(toc = TRUE) {
+
+  css <- system.file("rmarkdown/templates/report/resource/report.css", package = "basictemplate")
+
+  # call the base html_document function
+  rmarkdown::html_document(fig_width = 6.5,
+                           fig_height = 4,
+                           highlight = NULL,
+                           theme = NULL,
+                           css = css)
+}
