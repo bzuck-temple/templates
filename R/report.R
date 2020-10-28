167c32a88832420cb40e35127b74f888015fec47
bzuck <tuc50262@temple.edu>
2020-10-27 22:34:13 -0400

    fixing True

--- R/report.R
+++ R/report.R
@@ -4,8 +4,8 @@ JamiesStyle <- function(toc = TRUE) {
   css <- system.file("rmarkdown/templates/report/resource/report.css", package = "basictemplate")
 
   # call the base html_document function
-  rmarkdown::html_document(toc = true,
-        toc_float =  true,
+  rmarkdown::html_document(toc = TRUE,
+        toc_float =  TRUE,
         toc_depth = 1,
         css = css)
 }
