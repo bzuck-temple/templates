acb05bf701e0ffd620d9da51c78f7183200c27c9
bzuck <tuc50262@temple.edu>
2020-10-27 22:28:54 -0400

    changing html settings

--- R/report.R
+++ R/report.R
@@ -4,9 +4,9 @@ JamiesStyle <- function(toc = TRUE) {
   css <- system.file("rmarkdown/templates/report/resource/report.css", package = "basictemplate")
 
   # call the base html_document function
-  rmarkdown::html_document(fig_width = 6.5,
-                           fig_height = 4,
-                           highlight = NULL,
-                           theme = NULL,
-                           css = css)
+  rmarkdown::
+    html_document(toc = true,
+        toc_float=  true,
+        toc_depth= 1,
+        css = css)
 }
