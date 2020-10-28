c6e6366a6afc19ea421e05ae42d59a51b57ef673
bzuck <tuc50262@temple.edu>
2020-10-27 22:32:40 -0400

    blahhh

--- R/report.R
+++ R/report.R
@@ -4,9 +4,8 @@ JamiesStyle <- function(toc = TRUE) {
   css <- system.file("rmarkdown/templates/report/resource/report.css", package = "basictemplate")
 
   # call the base html_document function
-  rmarkdown::
-    html_document(toc = true,
-        toc_float=  true,
-        toc_depth= 1,
+  rmarkdown::html_document(toc = true,
+        toc_float =  true,
+        toc_depth = 1,
         css = css)
 }
